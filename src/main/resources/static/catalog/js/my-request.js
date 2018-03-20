0140a9d916bfa54ace7fff58120751aacab1f4e7
Jallappino <stefan.murakami@gmail.com>
2018-03-20 15:16:53 -0400

    browser controller test

---
+++ src/main/resources/static/catalog/js/my-request.js
@@ -0,0 +1,19 @@
+const xhr = new XHMLHttpRequest(); 
+xhr.onreadystatechange = function () {
+	if (xhe.readyState === 4 && xhr.status === 200) {
+		const res = JSON.parse(xhr.responseText) 
+		const productsContainer = document.querySelector('#products-container')
+
+		for (let product in res) {
+			const productContainer = document.createElement('div')
+			const productLink = document.createElement('a')
+			productLink.setAttribute('href', '/catalog/product.html');
+			productLink.innerText = product.name
+			productContainer.appendChild(productLink)
+			productsContainer.appendChild(productContainer)
+		}
+	}
+}
+
+xhr.open('GET', '/products',true)
+xhr.send()
